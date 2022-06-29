b7c5168d877a74fb0bb86a03cc45e289e5e2e82b
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-06-29 17:50:22 -0300

    Update funcoes_de_vetores.js

--- principais_functions/funcoes_de_vetores.js
+++ principais_functions/funcoes_de_vetores.js
@@ -1,10 +1,11 @@
 
-
+//CRIAR UM VETOR VAZIO DO TAMANHO ESCOLHIDO
 function novo_vetor(tamanho){
     const vetor = new Array(tamanho)
     return vetor
 }
 
+//MOSTRA O VETOR
 function show_vetor(vetor){
     print (vetor)
 }
@@ -13,11 +14,13 @@ function resetar_vetores(vetor){
 
 }
 
+//MOSTRA O NUMERO DE ELEMENTOS DO VETOR
 function qtd_itens_no_vetor(vetor){
     let qtd = vertor.length
     return (qtd)
 }
 
+//RETORNA O MENOR VALOR DO VETOR E SUA POSIÇAÕ
 function menor_valor(vetor){
     let menor = vetor[0]
     let posicao_do_menor = 0
@@ -32,6 +35,7 @@ function menor_valor(vetor){
     return (`O menor valor do vetor é ${menor} na posição ${posicao_do_menor} .`)
 }
 
+//RETORNA O MAIOR VALOR E SUA POSIÇÃO
 function maior_valor(vetor){
     let maior = vetor[0]
     let posicao_do_maior = 0
@@ -46,6 +50,7 @@ function maior_valor(vetor){
     return (`O maior valor do vetor é ${maior} na posição ${posicao_do_maior} .`)
 }
 
+//RETORNA A MEDIA DOS VALORES DO VETOR
 function media_valores_vetor(vetor){
     let media = soma_dos_valores/vetor.length
     let soma_dos_valores = 0
@@ -56,6 +61,7 @@ function media_valores_vetor(vetor){
     return(media)
 }
 
+//RETORNA A SOMA DOS VALORES
 function soma_dos_valores(vetor){
     let soma_dos_valores = 0
     for(i = 0; i < vetor.length ; i++){
@@ -65,6 +71,7 @@ function soma_dos_valores(vetor){
     return(soma_dos_valores)
 }
 
+//RETORNA APENAS OS VALORES NEGATIVOS E A QUANTIDADE DELES
 function valores_negativos(vetor){
     let itens_negativos = []
     let qtd_itens_negativos = 0
@@ -77,6 +84,7 @@ function valores_negativos(vetor){
     return(`Existem ${qtd_itens_negativos} itens negativos no vetor e são ${itens_negativos}`)
 }
 
+//RETORNA OS POSITIVOS E A QUANTIDADE 
 function valores_positivos(vetor){
     let itens_positivos = []
     let qtd_itens_positivos = 0
@@ -88,6 +96,7 @@ function valores_positivos(vetor){
     }
     return(`Existem ${qtd_itens_positivos} itens positivos no vetor e são ${itens_positivos}`)
 }
+
 
 function add_item_no_vetor(vetor2,item){
     vetor2[vetor2.length] = item
