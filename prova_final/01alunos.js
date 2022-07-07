e17075f720a6855dbe27825f70dfe1c9671efaa2
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-07-06 23:49:33 -0300

    Update 01alunos.js

--- prova_final/01alunos.js
+++ prova_final/01alunos.js
@@ -12,10 +12,10 @@ c. O número de aprovados;
 d. O número reprovados;
 e. O número de prova final.
 f. Reprovados por falta.*/
-import * as fs from 'fs'
+import fs from 'fs'
 
 function main(){
-    const dados_de_alunos = loadFile("prova_final\alunos.txt").split('\n')
+    const dados_de_alunos = fs.readFileSync('./alunos.txt','utf-8').split('\n')
 
     let media_do_aluno = 0
     let soma_das_medias = 0
@@ -27,9 +27,9 @@ function main(){
     for (let dado of dados_de_alunos){
         let dados_divididos = dado.split(';')
         let nome = dados_divididos[0]
-        let nota_1 = dados_divididos[1]
-        let nota_2 = dados_divididos[2]
-        let nota_3 = dados_divididos[3]
+        let nota_1 = Number(dados_divididos[1])
+        let nota_2 = Number(dados_divididos[2])
+        let nota_3 = Number(dados_divididos[3])
         let faltas = dados_divididos[4]
 
         faltas = faltas.split('/').map(Number)
@@ -38,42 +38,30 @@ function main(){
 
         media_do_aluno = (nota_1 + (nota_2 * 2) + (nota_3 * 3)) / 6
         soma_das_medias = soma_das_medias + media_do_aluno
-        console.log(`A média do(a) ${nome} é ${media_do_aluno}`)
-
+        console.log(`A média do(a) ${nome} é ${media_do_aluno.toFixed(2)}`)
+    
         if((ausencias/aulas_totais) > 25){
-            console.log('REPROVADO POR FALTAS!!!')
             contador_reprovados_por_falta = contador_reprovados_por_falta + 1
         } else {
             if(media_do_aluno > 7){
-                console.log('ALUNO APROVADO!!!')
                 contador_aprovados = contador_aprovados + 1
             } else if (media_do_aluno < 4){
-                console.log('REPROVADO!!!')
                 contador_reprovados++
             } else {
-                console.log('ALUNO DE PROVA FINAL!!!')
                 contador_prova_final++
             }
-
-            let media_da_turma = soma_das_medias/dados_divididos.length
-            console.log(`A média da turma foi ${media_da_turma}`)
+        }
+    }
+            const media_da_turma = soma_das_medias/dados_de_alunos.length
+            console.log('\nRESUMO DAS NOTAS')
+            console.log(`A média da turma foi ${media_da_turma.toFixed(2)}`)
             console.log(`A turma teve ${contador_aprovados} alunos aprovados.`)
             console.log(`A turma teve ${contador_reprovados} alunos reprovados.`)
             console.log(`A turma teve ${contador_prova_final} alunos de prova final.`)
             console.log(`A turma teve ${contador_reprovados_por_falta} alunos reprovados por falta.`)
-        }
-
-
-    }
+        
 }
 
-function loadFile(lista){
-    try{
-        const data = fs.readFileSync(lista, 'utf-8')
-        return data
-    } catch (error){
-        console.error(error)
-    }
-}
+
 
 main()
