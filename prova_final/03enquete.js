b45baf0d6172387880f1727bd960babdf7cb1cb6
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2022-07-07 19:03:42 -0300

    Update 03enquete.js

--- prova_final/03enquete.js
+++ prova_final/03enquete.js
@@ -4,12 +4,12 @@ function main(){
     const qtd_candidatos = Number(input('quantos candidatos:'))
     const vetor_candidatos = novo_vetor(qtd_candidatos)
     const votos = novo_vetor_zerado(qtd_candidatos+3)
-    mostrar_menu(vetor_candidatos,qtd_candidatos)
+    let opcao = mostrar_menu(vetor_candidatos,qtd_candidatos)
     let qtd_votos = 0
-
+   
     while(opcao!=-1){
         qtd_votos++
-        let opcao = Number(input('DIGA SEU VOTO:'))
+        opcao = Number(input('DIGA SEU VOTO:'))
         votos[opcao]++
     }
 
@@ -17,21 +17,21 @@ function main(){
     for (let i = 0; i < porcentagem.length ; i++){
         porcentagem[i] = votos[i]*100/qtd_votos
     }
-    
+
     const numero = maior_valor(votos)
-    const vai_ter = segundo_turno(qtd_candidatos,votos,numero) 
+    const vai_ter = segundo_turno(qtd_candidatos,votos,numero)
+
     console.log('\nRESUMO')
     console.log(`foram entrevistadas ${qtd_votos} pessoas.`)
-    
     for (let i = 0 ; i < qtd_candidatos ; i++){
-    console.log(`os votos no candidato ${vetor_candidatos[i]} foram ${votos[i]} que equivale a ${porcentagem[i]}% do total.`)
-    }
-    console.log(`os votos em branco foram ${votos[votos.length - 2]} que equivale a ${porcentagem[porcentagem.length - 2]}% do total.`)
-    console.log(`os votos em nulo foram ${votos[votos.length - 1]} que equivale a ${porcentagem[porcentagem.length - 1]}% do total.`)
-    console.log(`os votos em nao opinaram foram ${votos[votos.length]} que equivale a ${porcentagem[porcentagem.length]}% do total.`)
-    console.log(`o lider da enquete foi ${vetor_candidatos[numero]}.`)
-    console.log(`de acordo com a pesquisa ${vai_ter} ter segundo turno`)
-    
+        console.log(`os votos no candidato ${vetor_candidatos[i]} foram ${votos[i]} que equivale a ${porcentagem[i]}% do total.`)
+        }
+        console.log(`os votos em branco foram ${votos[votos.length - 2]} que equivale a ${porcentagem[porcentagem.length - 2]}% do total.`)
+        console.log(`os votos em nulo foram ${votos[votos.length - 1]} que equivale a ${porcentagem[porcentagem.length - 1]}% do total.`)
+        console.log(`os votos em nao opinaram foram ${votos[votos.length]} que equivale a ${porcentagem[porcentagem.length]}% do total.`)
+        console.log(`o lider da enquete foi ${vetor_candidatos[numero]}.`)
+        console.log(`de acordo com a pesquisa ${vai_ter} ter segundo turno`)
+
 }
 
 function novo_vetor(tamanho){
@@ -39,6 +39,7 @@ function novo_vetor(tamanho){
     for(let i = 0; i < vetor.length ; i++){
         vetor[i] = input('qual o nome do candidato:')
     }
+    return (vetor)
 }
 
 function novo_vetor_zerado(tamanho){
@@ -46,6 +47,7 @@ function novo_vetor_zerado(tamanho){
     for(let i = 0; i < vetor.length ; i++){
         vetor[i] = 0 
     }
+    return(vetor)
 }
 
 function mostrar_menu(vetor_candidatos,qtd_candidatos){
@@ -53,7 +55,7 @@ function mostrar_menu(vetor_candidatos,qtd_candidatos){
     let menu = '-----votacao------\n'
     
     for ( let i = 0 ; i < qtd_candidatos ; i++){
-      console.log (menu += 'i - candidato',vetor_candidatos[i],'\n')
+       menu += `${i} - ${vetor_candidatos[i]}\n`
     }
     menu += `${qtd_candidatos} - BRANCO \n`
     menu += `${qtd_candidatos + 1} - NULO \n`
@@ -66,14 +68,14 @@ function mostrar_menu(vetor_candidatos,qtd_candidatos){
 
     return opcao
 }
-    
+
 function maior_valor(votos){
     let maior = votos[0]
     let posicao_do_maior = [0]
 
-    for(i = 0;i < vetor.length; i++){
-        if(vetor[i] > maior){
-            maior = vetor[i]
+    for(let i = 0;i < votos.length; i++){
+        if (votos[i] > maior){
+            maior = votos[i]
             posicao_do_maior = [i]
         }
     }
@@ -84,14 +86,14 @@ function maior_valor(votos){
 function segundo_turno(numero,votos,qtd_candidatos){
     const maior = votos [numero]
     let contador_maior = 0
-    for(i=0 ; i < qtd_candidatos ; i++){
+    for(let i=0 ; i < qtd_candidatos ; i++){
          if (votos[i] === maior){
                 contador_maior++
           }
      if (contador_maior > 1){
-          Return ('vai')
+          return ('vai')
         } else {
-          Return ('nao vai')
+          return ('nao vai')
         }
     }
 }
